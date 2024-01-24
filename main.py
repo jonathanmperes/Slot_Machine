9a13c0f4fd4a9f1b9db592f9c0e9ac12b2f7defc
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-24 15:24:09 -0300

    function get_number_of_lines

--- main.py
+++ main.py
@@ -15,6 +15,20 @@ def deposite():
 
   return amount
 
+def get_number_of_lines():
+  while True:
+    lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+    if lines.isdigit():
+      lines = int(lines)
+      if MIN_LINES <= lines <= MAX_LINES:
+        break
+      else:
+        print("Enter a valid number of lines.")
+    else:
+      print("Please enter a number.")
+
+  return lines
+
 def main():
   amount = deposite()
 
