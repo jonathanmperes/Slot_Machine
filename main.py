d66b93a0bc26b530f13350f379b1906bf3da1b8f
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-25 09:21:32 -0300

    total_bet set and info printed

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ def main():
   balance = deposite()
   lines = get_number_of_lines()
   bet = get_bet()
-  
-  print(balance, lines, bet)
+  total_bet = bet * lines
+
+  print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}.")
 
 main()
