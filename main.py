104d952a0de5fb9a6e24bdca893de57eb0481bce
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-27 09:47:20 -0300

    function check_winnings updated with winning lines returned

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ symbol_value = {
 
 def check_winnings(columns, lines, bet, values):
   winnings = 0
+  winning_lines = []
   for line in range(lines):
     symbol = columns[0][line]
     for column in columns:
@@ -33,7 +34,8 @@ def check_winnings(columns, lines, bet, values):
       if symbol != symbol_to_check:
         break
     else:
-      winnings += values[symbol] *bet
+      winnings += values[symbol] * bet
+      winning_lines.append(line + 1)
   
   return winnings
 
